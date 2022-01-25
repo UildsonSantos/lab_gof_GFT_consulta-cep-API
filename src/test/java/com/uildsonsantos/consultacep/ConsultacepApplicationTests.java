dba3c48593fef51e49b30d5abee36ac04acf0b30
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-01-25 16:26:06 -0300

    first commit

---
+++ src/test/java/com/uildsonsantos/consultacep/ConsultacepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uildsonsantos.consultacep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultacepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
